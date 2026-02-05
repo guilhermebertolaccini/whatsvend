7ee0a08dc9c2f0ce57b2a18a6e4c8284d11e7123
DanielCayresFilho <dccf.virtualcontato@gmail.com>
2026-02-05 12:19:56 -0300

    templates

--- backend/src/templates/dto/update-template.dto.ts
+++ backend/src/templates/dto/update-template.dto.ts
@@ -1,5 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTemplateDto } from './create-template.dto';
+import { IsOptional, IsString, IsEnum } from 'class-validator';
 
-export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {}
+export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {
+    @IsOptional()
+    @IsString()
+    @IsEnum(['APPROVED', 'PENDING', 'REJECTED'])
+    status?: 'APPROVED' | 'PENDING' | 'REJECTED';
+}
 
